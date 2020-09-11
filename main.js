3000d79f1c7546009f4adef753bdca86cbce789d
Artem Potapchick <49004382+ArtemPotapchick@users.noreply.github.com>
2020-09-11 14:00:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -28,7 +28,8 @@ function getQuantityElements(heightElement){
 
 function startGame(){
     start.classList.add('hide');
-
+    gameArea.innerHTML = '';
+   
     for(let i = 0; i < getQuantityElements(100); i++){
         const line = document.createElement('div');
         line.classList.add('line');
@@ -46,16 +47,20 @@ function startGame(){
         enemy.style.background = 'transparent url("./image/newenemy.png") center / cover no-repeat'
         gameArea.appendChild(enemy);
     }
-
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
+    car.style.left = gameArea.offsetWidth/2 - car.offsetWidth/2 ;
+    car.style.top = 'auto';
+    car.style.bottom = '10px';
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
 
 }
 function playGame(){
-   
+    setting.score+=setting.speed ;
+    score.innerHTML = 'SCORE<br>'+ setting.score;
     if(setting.start) {
         moveRoad();
         moveEnemy();
@@ -105,6 +110,20 @@ function moveRoad(){
 function moveEnemy(){
     let enemy = document.querySelectorAll('.enemy');
     enemy.forEach(function(item){
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = item.getBoundingClientRect();
+
+        if(carRect.top <= enemyRect.bottom && 
+            carRect.right >= enemyRect.left &&
+            carRect.left <= enemyRect.right &&
+            carRect.bottom >= enemyRect.top){
+            
+            setting.start = false;
+            console.warn('ДТП');
+            start.classList.remove('hide');
+            start.style.top = score.offsetHeight;
+        }
+
         item.y += setting.speed /2 ;
         item.style.top = item.y + 'px';
 
